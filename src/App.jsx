139fbbe0c1a1c5b90b029ddf322e8a3afb7559a3
retagshobeka <retagshobeka19@gmail.com>
2026-05-03 21:34:55 +0300

    final project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+function App() {
+  return (
+    <div>
+      <h1>ShopSmart Team</h1>
+
+      <Login />
+
+      <hr />
+
+      <Register />
+    </div>
+  );
+}
+
+export default App;
