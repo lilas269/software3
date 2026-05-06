161163675abb79f3610a3d3ac745dda468d7ef20
lilas269 <lilasdosokidosoki@gmail.com>
2026-05-06 13:45:01 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ProductCatalog from "./pages/ProductCatalog";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Product Catalog</h1>
+      <ProductCatalog />
+    </div>
+  );
+}
+
+export default App;
